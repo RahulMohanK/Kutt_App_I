d8d9378da9364eec5c3505cee36de55a082b2a4f
RahulMohanK <rahulmohan1999@gmail.com>
2018-04-30 02:18:29 +0530

    first

---
+++ app/src/main/java/com/example/root/kutt_app_i/ListenItem.java
@@ -0,0 +1,17 @@
+package com.example.root.kutt_app_i;
+
+public class ListenItem {
+    private String Link;
+
+
+    public ListenItem(String link ) {
+        Link = link;
+
+    }
+
+    public String getLink(){
+        return Link;
+    }
+
+
+}
