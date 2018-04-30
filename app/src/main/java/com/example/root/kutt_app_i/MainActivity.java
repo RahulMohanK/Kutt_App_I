0bc18ceab005754b5949b9025dd5ea76af1ced81
philippaul1998 <pippu98@gmail.com>
2018-04-30 23:52:04 +0530

    Remove link done,copy to clipboard done,few ui updates

---
+++ app/src/main/java/com/example/root/kutt_app_i/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.root.kutt_app_i;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    DatabaseHelper myDb;
+
+    private static Button  show_text,got;
+    private static TextView clipboardData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
+       //show_text = (Button) findViewById(R.id.show_text);//
+
+        got = (Button) findViewById(R.id.button);
+
+       //clipboardData = (TextView) findViewById(R.id.clipboard_data);//
+
+       got.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               Intent ii = new Intent(MainActivity.this,BackgroundActivity.class);
+               startActivity(ii);
+           }
+       });
+
+
+
+        //showData();//
+
+
+    }
+
+
+
+
+
+   /* public void showData( ){
+
+        show_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Cursor res = myDb.getAllData();
+
+                if(res.getCount()==0)
+                {
+                    showMessage("Error","Nothing Found");
+                    return;
+                }
+
+                StringBuffer stringBuffer =new StringBuffer();
+
+                while(res.moveToNext()){
+
+                    stringBuffer.append("Link : "+ res.getString(1)+"\n\n\n");
+
+                }
+
+                showMessage("Data",stringBuffer.toString());
+            }
+        });
+
+    }*/
+
+
+
+   /* public void showMessage(String title,String message){
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.show();
+
+
+
+    }*/
+
+
+
+
+}
---
+++ app/src/main/java/com/example/root/kutt_app_i/MyAdapter.java
@@ -0,0 +1,143 @@
+package com.example.root.kutt_app_i;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * Created by RahulKMohan on 07-01-2018.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+    private List<ListenItem> listenItems;
+
+
+    Context context;
+    int lastPosition = -1;
+
+
+    public MyAdapter(List<ListenItem> listenItems, Context context) {
+        this.listenItems = listenItems;
+        this.context = context;
+    }
+
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_listen, parent, false);
+        return new ViewHolder(v, context, listenItems);
+    }
+
+
+    @Override
+    public void onBindViewHolder(final MyAdapter.ViewHolder holder, final int position) {
+
+        final ListenItem listen = listenItems.get(position);
+
+        holder.Name.setText(listen.getLink());
+
+        holder.clip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = listen.getLink();
+                Clipboard_Utils.copyToClipboard(context,link);
+                //Toast.makeText(context,link,Toast.LENGTH_LONG).show();
+            }
+        });
+        holder.del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.linearLayout.setVisibility(View.GONE);
+                holder.confirm.setVisibility(View.VISIBLE);
+            }
+        });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHelper myDb = new DatabaseHelper(context);
+                String link = listen.getLink();
+                //Toast.makeText(context, position, Toast.LENGTH_SHORT).show();
+                if(myDb.deletelink(link)){
+                    holder.linearLayout.setVisibility(View.GONE);
+                    holder.confirm.setVisibility(View.GONE);
+                    Toast.makeText(context, "Link Removed Successfully!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        holder.cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.confirm.setVisibility(View.GONE);
+                holder.linearLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        /*if (position > lastPosition) {
+
+            Animation animation = AnimationUtils.loadAnimation(context,
+                    R.anim.item_animation_fall_down);
+            holder.itemView.setAnimation(animation);
+            lastPosition = position;
+        }*/
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listenItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder  {
+
+        private TextView Name, Phone;
+        public LinearLayout linearLayout,confirm;
+        private ImageView clip,del;
+        private Button cancel,delete;
+
+
+        public ViewHolder(final View ItemView, Context context, List<ListenItem> listenItems) {
+            super(ItemView);
+
+            confirm = ItemView.findViewById(R.id.confirm);
+            delete = ItemView.findViewById(R.id.delete);
+            cancel = ItemView.findViewById(R.id.cancel);
+            del = ItemView.findViewById(R.id.del);
+            clip = ItemView.findViewById(R.id.clip);
+            Name = (TextView) ItemView.findViewById(R.id.textViewName);
+
+            linearLayout=(LinearLayout)ItemView.findViewById(R.id.linearlayout);
+        }
+
+
+
+    }
+}
