9382c1d128ec64d416563fce6a1038eba3c73a55
RahulMohanK <rahulmohan1999@gmail.com>
2018-07-15 15:16:15 +0530

    Pager activity2

--- app/src/main/java/com/example/root/kutt_app_i/Dis.java
+++ app/src/main/java/com/example/root/kutt_app_i/Dis.java
@@ -52,8 +52,8 @@ public class Dis extends AppCompatActivity{
 
 
             this.data[i]=listenItems.get(i).getLink();
-            PageAdapter viewPageAdapter = new PageAdapter(getSupportFragmentManager(), data);
-            viewPager.setAdapter(viewPageAdapter);
+           /* PageAdapter viewPageAdapter = new PageAdapter(getSupportFragmentManager(), data);
+            viewPager.setAdapter(viewPageAdapter);*/
         }
 
     }
